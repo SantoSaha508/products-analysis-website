9ed96ba09b4eaa6879eb768068b5bc16abd2cb31
shanto <santo15-10959@diu.edu.bd>
2022-04-04 16:50:04 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header-container'>
+            <h3>Annapurna Bastralay.com</h3>
+            <a href="/home">Home</a>
+            <a href="/dashboard">DashBoard</a>
+            <a href="/review">Review</a>
+            <a href="/blogs">Blogs</a>
+            <a href="/about">About</a>
+        </div>
+    );
+};
+
+export default Header;
